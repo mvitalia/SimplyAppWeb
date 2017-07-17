0bff00f66f98577aab8e1cacf3d2f526de5594b5
mvitalia <p.guglielmo@mvitalia.com>
2017-07-17 19:14:06 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -37,6 +37,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        alert("pronto");
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
